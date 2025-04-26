da725de3500fec51f18cbcfe98552d64333c2603
jhernandez4 <jokhernandez120744@gmail.com>
2025-04-25 20:30:05 -0700

    feat(app page): call backend to fetch users

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { Search, X } from 'lucide-react'
+import axios from 'axios';
 
 function App() {
-  const backendUrl = import.meta.env.BACKEND_URL;
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const [value, setValue] = useState("");
+  const [usersList, setUsersList] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        setIsLoading(true);
+        const response = await axios.get(`${backendUrl}/users`);
+        setUsersList(response.data);
+        console.log(response.data);
+      } catch(error) {
+        setError(error?.response?.data?.message || 'Failed to fetch users')
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchUsers();
+  }, [])
 
   return (
     <div className="users-page">
